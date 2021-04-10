e199ed6b78a1fe2085b5323ff6c65b1ea26c1d5a
ibrahimbozkurt0 <ibrahimbozkurto@hotmail.com>
2021-04-10 21:14:34 +0300

    Adding entties.

---
+++ src/Domain/Entities/FuelType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+     public  class FuelType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+
+    }
+}
